7bb3486fa9b76e9b2b8f779359ea472c39e8152f
Janybek <best.joni.547545@gmail.com>
2022-02-19 03:48:38 +0600

    i created page E404

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,29 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 import Cart from '../views/Cart.vue'
+import E404 from '../views/E404.vue'
 
 const routes = [
   {
     path: '/',
     name: 'Home',
-    component: Home
+    component: Home,
   },
   {
     path: '/cart',
     name: 'Cart',
-    component: Cart
+    component: Cart,
+  },
+  {
+    path: '/:pathMatch(.*)',
+    name: 'E404',
+    component: E404,
   },
 ]
 
 const router = createRouter({
   history: createWebHashHistory(),
-  routes
+  routes,
 })
 
 export default router
